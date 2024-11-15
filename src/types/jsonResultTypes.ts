d4708cdb207dfc6f2507c42c426d09f3855b19d7
masaishi <mwishiha@ucsc.edu>
2024-11-15 20:45:29 +0900

    Add TranscriptEntry type

--- src/types/jsonResultTypes.ts
+++ src/types/jsonResultTypes.ts
@@ -1,7 +1,13 @@
 export interface JsonResult {
   accents: Record<string, number>;
   fillers: number;
-  transcript: string;
+  transcript: TranscriptEntry[];
   token_scores: Record<string, number>;
   mean_score: number;
+}
+
+export interface TranscriptEntry {
+  start_t: string;
+  end_t: string;
+  text: string;
 }
